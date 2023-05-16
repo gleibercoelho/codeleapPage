24c09874bf1820c38c6eb71dd1a4a4dc4cc0d342
GFC <gleiberfelipe@hotmail.com>
2023-05-16 02:29:48 -0300

    updade dois

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -36,7 +36,7 @@ const Login = () => {
     sessionStorage.setItem("userData", JSON.stringify(userData));
 
     // Redirect to the home page
-    window.location.href = "/posts";
+    window.location.href = "codeleapPage/posts";
   };
 
   return (
